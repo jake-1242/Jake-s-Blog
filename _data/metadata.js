e42e03a71087c2f6a3fd63e6d4ee06a8ae2f2f0f
Jake <jake1009@live.co.uk>
2023-09-12 00:43:25 +0100

    added a comma to correct a problem on the metadata page

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Eleventy Base Blog v8",
 	url: "https://vermillion-pika-806ec8.netlify.app/",
 	language: "en",
-	description: "A blog to share my projects and ideas about coding"
+	description: "A blog to share my projects and ideas about coding",
 	author: {
 		name: "Jake Barry",
 		email: "jake1009@live.co.uk",
